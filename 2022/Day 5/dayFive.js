5457684c4cbb7f8a066f3c31a47b135b983a5ec4
Suraj Thotakura <31972379+SurajThotakura@users.noreply.github.com>
2022-12-06 20:09:24 +0530

    day 5 done

--- 2022/Day 5/dayFive.js
+++ 2022/Day 5/dayFive.js
@@ -2,4 +2,77 @@ const fs = require("fs");
 
 let crates = fs.readFileSync("./input.txt", "utf-8").split("\n");
 
-console.log(crates[5].split(''))
+
+// parsing moves info
+
+let moves = crates.slice(10).map((x) =>
+  x
+    .split(" ")
+    .filter((word) => word != "move" && word != "from" && word != "to")
+    .map((x) => parseInt(x))
+);
+
+
+// parsing stacks of crates
+
+const createStacks = (array) => {
+  let stacks = [[], [], [], [], [], [], [], [], []];
+  for (let i = 7; i >= 0; i--) {
+    const element = array[i].split(" ");
+    let count = 0;
+    let stack = 0;
+    element.map((x) => {
+      if (!x) {
+        count++;
+        if (count === 4) {
+          count = 0;
+          stack++;
+        }
+      } else {
+        stacks[stack].push(x);
+        stack++;
+      }
+    });
+  }
+  return stacks;
+};
+
+let stacks = createStacks(crates);
+
+
+// Operate crane
+
+const applyMoves = (stacks, moves) => {
+  moves.map((x) => {
+    const ammount = x[0];
+    const origin = x[1] - 1;
+    const destination = x[2] - 1;
+
+    // romove reverse for part 2
+    const carrier = stacks[origin].slice(-ammount).reverse();
+
+    stacks[origin] = stacks[origin].slice(0, stacks[origin].length - ammount);
+    stacks[destination] = stacks[destination].concat(carrier);
+  });
+  return stacks;
+};
+
+
+const finalStacks = applyMoves(stacks, moves);
+
+
+// Get the top items
+
+const cream = (stacks) => {
+  let result = "";
+  stacks.map((x) => {
+    result = result + x.pop();
+  });
+  return result
+    .split("")
+    .filter((word) => word != "[" && word != "]")
+    .toString()
+    .replace(/,/g, "");
+};
+
+console.log(cream(finalStacks));
